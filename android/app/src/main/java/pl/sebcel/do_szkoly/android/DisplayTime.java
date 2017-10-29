57c4b2566ca7dd4f7bf265d9b4fafa2b8d850dd2
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2017-10-29 09:45:47 +0100

    Using schedule engine in Android application

--- android/app/src/main/java/pl/sebcel/do_szkoly/android/DisplayTime.java
+++ android/app/src/main/java/pl/sebcel/do_szkoly/android/DisplayTime.java
@@ -1,12 +1,24 @@
 package pl.sebcel.do_szkoly.android;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import pl.sebcel.do_szkoly.engine.Engine;
+import pl.sebcel.do_szkoly.engine.EventListener;
+import pl.sebcel.do_szkoly.engine.TimeInformation;
 
 public class DisplayTime extends AppCompatActivity {
+
+    private Engine engine;
+    private DateFormat df = new SimpleDateFormat("H:mm");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +27,34 @@ public class DisplayTime extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//                ((TextView) findViewById(R.id.currentTimeInfo)).setText("Elemele-dutki");
+        engine = new Engine();
+        engine.addEvent("10:00", "Ubieranie się");
+        engine.addEvent("10:30", "Wychodzenie z domu");
+        engine.addEvent("10:43", "Autobus 268");
+        engine.addEvent("11:00", "W szkole");
+
+        engine.addEventListener(new EventListener() {
+            @Override
+            public void handleTimeEvent(final TimeInformation timeInformation) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ((TextView) findViewById(R.id.currentTimeInfo)).setText(df.format(timeInformation.getCurrentTime()));
+                        if (timeInformation.getNextEventTime() != null) {
+                            ((TextView) findViewById(R.id.nextEventTime)).setText(df.format(timeInformation.getNextEventTime()));
+                            ((TextView) findViewById(R.id.nextEventInfo)).setText(timeInformation.getNextEvent());
+                            ((TextView) findViewById(R.id.timeToNextEventInfo)).setText("" + timeInformation.getTimeToNextStepInMinutes() + " min");
+                            ((TextView) findViewById(R.id.timeToNextEventInfo)).setTextColor(getTimeToNextEventColour(timeInformation.getTimeToNextStepInMinutes()));
+                        } else {
+                            ((TextView) findViewById(R.id.nextEventInfo)).setText("-");
+                            ((TextView) findViewById(R.id.timeToNextEventInfo)).setText("-");
+                        }
+                    }
+                });
+            }
+        });
+
+        engine.start();
     }
 
     @Override
@@ -33,5 +72,17 @@ public class DisplayTime extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private int getTimeToNextEventColour(long timeToNextEvent) {
+        if (timeToNextEvent < 2) {
+            return Color.RED;
+        }
+
+        if (timeToNextEvent < 5) {
+            return Color.YELLOW;
+        }
+
+        return Color.GREEN;
     }
 }
