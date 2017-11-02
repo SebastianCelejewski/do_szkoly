c39b542907b6d1f03f782cedc68388a4a4ab954d
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2017-11-02 09:25:56 +0100

    DisplaySchedule activity now displays the schedule. It looks poor, though

--- android/app/src/main/java/pl/sebcel/do_szkoly/android/DisplaySchedule.java
+++ android/app/src/main/java/pl/sebcel/do_szkoly/android/DisplaySchedule.java
@@ -6,8 +6,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+import pl.sebcel.do_szkoly.engine.Engine;
+import pl.sebcel.do_szkoly.engine.EventListener;
+import pl.sebcel.do_szkoly.engine.TimeInformation;
 
 public class DisplaySchedule extends AppCompatActivity {
+
+    private Engine engine = new Engine();
+    private DateFormat df = new SimpleDateFormat("H:mm");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +28,39 @@ public class DisplaySchedule extends AppCompatActivity {
         setContentView(R.layout.activity_display_schedule);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        engine = new Engine();
+        engine.addEvent("9:00", "Ubieranie się");
+        engine.addEvent("9:30", "Wychodzenie z domu");
+        engine.addEvent("9:43", "Autobus 268");
+        engine.addEvent("10:00", "W szkole");
+
+        engine.addEventListener(new EventListener() {
+            @Override
+            public void handleTimeEvent(final TimeInformation timeInformation) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        TextView completedStepsView = ((TextView) findViewById(R.id.completedSteps));
+                        TextView currentStepView = ((TextView) findViewById(R.id.currentStep));
+                        TextView outstandingStepsView = ((TextView) findViewById(R.id.outstandingSteps));
+
+                        String completedStepsText = "";
+                        for (Map.Entry<Date, String> step : timeInformation.getOutstandingEvents().entrySet()) {
+                            completedStepsText += stepToString(step) + "\n";
+                        }
+
+                        completedStepsView.setText(completedStepsText);
+                    }
+                });
+            }
+        });
+
+        engine.start();
+    }
+
+    private String stepToString(Map.Entry<Date, String> step) {
+        return df.format(step.getKey()) + "\t" + step.getValue();
     }
 }
